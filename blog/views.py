c6660ed4039180e12df0f6626cd6596248c1f77f
judiana <yoditfentaw90@gmail.com>
2024-07-07 00:55:46 +0200

    --

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Yodit',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'June 29, 2024'
-    },
-{
-        'author': 'Kaleab',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'June 30, 2024'
-    }
-
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
